ea7af70c1805543521e27666ac4d85a45e6a97cc
Tu Nombre <usuario@ejemplo.com>
2025-05-16 11:55:03 -0300

    UPDATE: ajustar las importaciones en el componente StatusBadge para mejorar la organización del código, cambiando la ruta de BadgeProps y añadiendo STATUS_COLORS y STATUS_LABELS desde la interfaz correspondiente. Se mejora la consistencia en la estructura del proyecto.

--- src/components/atoms/StatusBadge.tsx
+++ src/components/atoms/StatusBadge.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BadgeProps } from '@/interfaces/components/atoms/Badge.interfaces';
+import { BadgeProps } from '../../interfaces/components/atoms/Badge.interface';
+import { STATUS_COLORS, STATUS_LABELS } from '../../interfaces/types/ApplicationStatus.type';
 
-
-  const StatusBadge: React.FC<BadgeProps> = ({ status, className = '' }) => {
+const StatusBadge: React.FC<BadgeProps> = ({ status, className = '' }) => {
   return (
     <span
       className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${STATUS_COLORS[status]} ${className}`}
