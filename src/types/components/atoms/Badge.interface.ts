f3c3713fd4f5850fadec5957571094953dbe730f
Tu Nombre <usuario@ejemplo.com>
2025-05-20 04:04:11 -0300

    feat: actualizar interfaz de Badge para incluir nuevas propiedades
    - Se modificó la interfaz BadgeProps para añadir las propiedades 'variant', 'size' y 'onClick', mejorando la flexibilidad del componente.
    - Se ajustó la importación de PostulationStatus para reflejar la nueva estructura de carpetas.

--- src/types/components/atoms/Badge.interface.ts
+++ src/types/components/atoms/Badge.interface.ts
@@ -1,6 +1,9 @@
-import { PostulationStatus } from '../../../types/interface/postulations/postulation';
+import { PostulationStatus } from '../../interface/postulations/postulation';
 
 export interface BadgeProps {
   status: PostulationStatus;
   className?: string;
+  variant?: 'default' | 'outline' | 'secondary';
+  size?: 'default' | 'sm' | 'lg';
+  onClick?: () => void;
 }
