e1e48d30e56621b60eb4086c70fb1f3c6035ce19
Tu Nombre <usuario@ejemplo.com>
2025-05-20 04:08:53 -0300

    feat: corregir importación de BadgeProps en componentes atómicos
    - Se ajustó la importación de BadgeProps en index.ts para utilizar la nueva estructura de archivos, mejorando la organización del código.
    - Se eliminaron exportaciones redundantes de BadgeProps, ButtonProps y AvatarProps, optimizando el archivo.

--- src/types/components/atoms/index.ts
+++ src/types/components/atoms/index.ts
@@ -1,7 +1,6 @@
 import { ButtonProps } from './ButtonProps.interface';
 import { AvatarProps } from './Avatar.interface';
-import { BadgeProps } from './badges/Badge.interfaces';
-
+import { BadgeProps } from './Badge.interface';
 
 // Exportar interfaces de componentes atómicos
 export type { ButtonProps, AvatarProps, BadgeProps };
@@ -9,8 +8,4 @@ export type { ButtonProps, AvatarProps, BadgeProps };
 export * from './ButtonProps.interface';
 export * from './Avatar.interface';
 export * from './badges/index';
-
-export type { BadgeProps } from './Badge.interface';
-export type { ButtonProps } from './ButtonProps.interface';
-export type { AvatarProps } from './Avatar.interface';
 
