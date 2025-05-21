a27ebd3e3bb81e777c6b467181beb5eb4ede3bc9
Tu Nombre <usuario@ejemplo.com>
2025-05-21 12:12:12 -0300

    feat: ajustar estilos en FieldWrapper para mejorar presentación
    - Se eliminó la clase "block" del elemento label en el componente FieldWrapper, optimizando la presentación visual del formulario de autenticación.

--- src/features/auth/components/FieldWrapper.tsx
+++ src/features/auth/components/FieldWrapper.tsx
@@ -14,7 +14,7 @@ export const FieldWrapper: React.FC<FieldWrapperProps> = ({
   errorMessage
 }) => (
   <div className="relative">
-    <label htmlFor={name} className="block text-base font-semibold text-gray-700 dark:text-white mb-2 drop-shadow flex items-center gap-2">
+    <label htmlFor={name} className="text-base font-semibold text-gray-700 dark:text-white mb-2 drop-shadow flex items-center gap-2">
       {label} {required && <span className="text-red-500">*</span>}
       {tooltip && (
         <div className="group relative">
