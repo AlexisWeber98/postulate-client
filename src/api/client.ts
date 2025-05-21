60ce775a61f2e437470878c2220a1e982e691395
Tu Nombre <usuario@ejemplo.com>
2025-05-21 14:56:59 -0300

    feat: añadir configuración adicional para solicitudes HTTP en el cliente
    - Se incorporó AxiosRequestConfig en la importación del cliente HTTP, permitiendo una mayor personalización en las configuraciones de las peticiones.

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import { useAuthStore } from "../store/auth/authStore";
 
 // Configuración base para las peticiones HTTP
