63236fbf06dc412adb04c6493d32091735a15e2c
Tu Nombre <usuario@ejemplo.com>
2025-05-21 16:38:53 -0300

    feat: actualizar la interfaz AuthState para mejorar la gestión de datos de usuario
    - Se ha modificado el método updateUser en la interfaz AuthState para aceptar un objeto de tipo Partial<Omit<User, 'id'>>, lo que permite una actualización más flexible de los datos del usuario sin requerir el ID.

--- src/types/auth/auth.interface.ts
+++ src/types/auth/auth.interface.ts
@@ -26,7 +26,7 @@ export interface AuthState {
   signIn: (email: string, password: string) => Promise<void>;
   signUp: (email: string, password: string, name: string, userName: string, lastName: string) => Promise<void>;
   signOut: () => void;
-  updateUser: (data: { name?: string; email?: string }) => void;
+  updateUser: (data: Partial<Omit<User, 'id'>>) => void;
 }
 
 export interface LoginRequest {
