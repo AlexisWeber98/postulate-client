80512d23a7521ef4cfaa22d12038d633e09873aa
Tu Nombre <usuario@ejemplo.com>
2025-07-30 21:55:19 -0300

    refactor: mejorar la legibilidad y consistencia en index.ts y application.helpers.ts
    - Se mejoró la legibilidad del código al agregar saltos de línea en las exportaciones de tipos en index.ts.
    - Se estandarizó el formato de los mensajes de advertencia en application.helpers.ts para facilitar la lectura y el mantenimiento del código.

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,12 +1,7 @@
 // Components
 // - Cards
-import { FeatureCard } from '../types/components/cards/FeatureCard.interface'
-import { ThreeCardFocusCarouselProps } from '../types/components/cards/ThreeCardFocusCarouselProps.interface'
-import { ApplicationCardProps } from '../types/components/organisms/ApplicationCard.interface'
+import { FeatureCard } from '../types/components/cards/FeatureCard.interface';
+import { ThreeCardFocusCarouselProps } from '../types/components/cards/ThreeCardFocusCarouselProps.interface';
+import { ApplicationCardProps } from '../types/components/organisms/ApplicationCard.interface';
 
-export type {
-  FeatureCard,
-  ThreeCardFocusCarouselProps,
-  ApplicationCardProps,
-}
-
+export type { FeatureCard, ThreeCardFocusCarouselProps, ApplicationCardProps };
