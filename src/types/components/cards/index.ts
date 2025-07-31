fa3e04a970aa513db3535a99d911efedbd5fa50e
Tu Nombre <usuario@ejemplo.com>
2025-07-30 22:02:20 -0300

    refactor: mejorar la legibilidad y consistencia en interfaces de componentes
    - Se estandarizó el formato de las definiciones de tipos en Badge.interface.ts, ApplicationCardProps.interface.ts, FeatureCard.interface.ts y FeaturesCarousel.interface.ts.
    - Se eliminaron espacios en blanco innecesarios en varios archivos para mejorar la presentación del código.
    - Se mejoró la claridad de las exportaciones en index.ts de componentes atómicos y de tarjetas.

--- src/types/components/cards/index.ts
+++ src/types/components/cards/index.ts
@@ -1,4 +1,7 @@
-export type { FeaturesCarouselProps, Feature as FeatureCarousel } from './FeaturesCarouselProps.interface';
+export type {
+  FeaturesCarouselProps,
+  Feature as FeatureCarousel,
+} from './FeaturesCarouselProps.interface';
 export type { FeatureCard } from './FeatureCard.interface';
 export type { ThreeCardFocusCarouselProps } from './ThreeCardFocusCarouselProps.interface';
 export type { ApplicationCardProps } from './ApplicationCardProps.interface';
